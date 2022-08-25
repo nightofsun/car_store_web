1b61b429cb109d19c16764eac04a767516dba38d
natthaphon <natthaphon@mfec.co.th>
2022-08-25 18:18:44 +0700

    refactor sort constant

--- src/common/constant.js
+++ src/common/constant.js
@@ -16,40 +16,6 @@ export const brandList = [
     {value: BRAND_NISSAN, text: BRAND_NISSAN},
     {value: BRAND_BENZ, text: BRAND_BENZ},
 ]
-export const TYPE_1 = "รถเก๋ง"
-export const TYPE_2 = "รถกระบะ"
-export const TYPE_3 = "รถบรรทุก"
-export const TYPE_4 = "รถ SUV"
-export const TYPE_5 = "รถตู้"
-export const TYPE_SELECT = "โปรดเลือกประเภท"
-export const typeList = [
-    {value: '', text: TYPE_SELECT},
-    {value: TYPE_1, text: TYPE_1},
-    {value: TYPE_2, text: TYPE_2},
-    {value: TYPE_3, text: TYPE_3},
-    {value: TYPE_4, text: TYPE_4},
-    {value: TYPE_5, text: TYPE_5}
-]
-export const GEAR_MANUAL = "เกียร์ธรรมดา"
-export const GEAR_AUTO = "เกียร์ออโต้"
-export const GEAR_CVT = "เกียร์ออโต้ CVT"
-export const GEAR_SELECT = "โปรดเลือกเกียร์"
-export const gearList = [
-    {value: '', text: TYPE_SELECT},
-    {value: GEAR_MANUAL, text: GEAR_MANUAL},
-    {value: GEAR_AUTO, text: GEAR_AUTO},
-    {value: GEAR_CVT, text: GEAR_CVT},
-]
-export const SORT_TYPE_1 = "วันที่เพิ่มใหม่-เก่า"
-export const SORT_TYPE_2 = "วันที่เพิ่มเก่า-ใหม่"
-export const SORT_TYPE_3 = "ราคามาก-น้อย"
-export const SORT_TYPE_4 = "ราคาน้อย-มาก"
-export const sortList = [
-    {value: '0', text: SORT_TYPE_1},
-    {value: '1', text: SORT_TYPE_2},
-    {value: '2', text: SORT_TYPE_3},
-    {value: '3', text: SORT_TYPE_4},
-]
 export const RW_DRIVE = "ระบบขับเคลื่อนล้อหลัง"
 export const FW_DRIVE = "ระบบขับเคลื่อนล้อหน้า"
 export const FOURW_DRIVE = "ระบบขับเคลื่อน 4 ล้อ"
@@ -61,6 +27,16 @@ export const driveSystemList = [
     {value: FW_DRIVE, text: FW_DRIVE},
     {value: FOURW_DRIVE, text: FOURW_DRIVE},
     {value: AW_DRIVE, text: AW_DRIVE},
+]
+export const GEAR_MANUAL = "เกียร์ธรรมดา"
+export const GEAR_AUTO = "เกียร์ออโต้"
+export const GEAR_CVT = "เกียร์ออโต้ CVT"
+export const GEAR_SELECT = "โปรดเลือกเกียร์"
+export const gearList = [
+    {value: '', text: TYPE_SELECT},
+    {value: GEAR_MANUAL, text: GEAR_MANUAL},
+    {value: GEAR_AUTO, text: GEAR_AUTO},
+    {value: GEAR_CVT, text: GEAR_CVT},
 ]
 export const OIL_1 = "เบนซิน 95"
 export const OIL_2 = "เบนซิน 91"
@@ -80,4 +56,29 @@ export const oilList = [
     {value: OIL_6, text: OIL_6},
     {value: OIL_7, text: OIL_7},
 ]
+export const SORT_TYPE_1 = "วันที่เพิ่มใหม่-เก่า"
+export const SORT_TYPE_2 = "วันที่เพิ่มเก่า-ใหม่"
+export const SORT_TYPE_3 = "ราคามาก-น้อย"
+export const SORT_TYPE_4 = "ราคาน้อย-มาก"
+export const sortList = [
+    {value: '0', text: SORT_TYPE_1},
+    {value: '1', text: SORT_TYPE_2},
+    {value: '2', text: SORT_TYPE_3},
+    {value: '3', text: SORT_TYPE_4},
+]
+export const TYPE_1 = "รถเก๋ง"
+export const TYPE_2 = "รถกระบะ"
+export const TYPE_3 = "รถบรรทุก"
+export const TYPE_4 = "รถ SUV"
+export const TYPE_5 = "รถตู้"
+export const TYPE_SELECT = "โปรดเลือกประเภท"
+export const typeList = [
+    {value: '', text: TYPE_SELECT},
+    {value: TYPE_1, text: TYPE_1},
+    {value: TYPE_2, text: TYPE_2},
+    {value: TYPE_3, text: TYPE_3},
+    {value: TYPE_4, text: TYPE_4},
+    {value: TYPE_5, text: TYPE_5}
+]
+
 export const MAX_INTEGER_VALUE = "2147483648"
--- src/common/pathConstant.js
+++ src/common/pathConstant.js
@@ -1,12 +1,16 @@
-export const HOME_PATH = '/'
-export const HOME_NAME = 'home'
-export const EDIT_CAR_PATH = '/editCar'
-export const EDIT_CAR_NAME = 'editCar'
-export const ERROR_INTERNET_PATH = '/errorInternetConnection'
-export const ERROR_INTERNET_NAME = 'errorInternetConnection'
-export const SERVER_ERROR_PATH = '/serverError'
-export const SERVER_ERROR_NAME = 'serverError'
-export const API_GET_CAR_LIST = '/getCarList'
 export const API_CREATE_NEW_CAR = '/createNewCar'
-export const API_UPDATE_CAR = '/updateCar'
 export const API_DELETE_CAR = '/deleteCar'
+export const API_GET_CAR_LIST = '/getCarList'
+export const API_UPDATE_CAR = '/updateCar'
+export const EDIT_CAR_NAME = 'editCar'
+export const EDIT_CAR_PATH = '/editCar'
+export const ERROR_INTERNET_NAME = 'errorInternetConnection'
+export const ERROR_INTERNET_PATH = '/errorInternetConnection'
+export const HOME_NAME = 'home'
+export const HOME_PATH = '/'
+export const SERVER_ERROR_NAME = 'serverError'
+export const SERVER_ERROR_PATH = '/serverError'
+
+
+
+
