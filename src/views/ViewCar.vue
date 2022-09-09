573043c40e67c63053b9d83778d897686719e008
natthaphon <natthaphon@mfec.co.th>
2022-09-09 14:11:24 +0700

    Revert "add test image"
    This reverts commit da35893c7eb8f9db858ad935ea975883406f4e21.

--- src/views/ViewCar.vue
+++ src/views/ViewCar.vue
@@ -1,79 +1,64 @@
 <template>
-  <div>
-    <Search />
-    <img id="img-1" :src="img"/>
-    <div class="table-box">
-      <table class="table table-striped table-bordered">
-        <thead>
-          <tr>
-            <th>{{ $t('default.brand') }}</th>
-            <th>{{ $t('default.name') }}</th>
-            <th>{{ $t('default.carType') }}</th>
-            <th>{{ $t('default.driveSystem') }}</th>
-            <th>{{ $t('default.gearType') }}</th>
-            <th>{{ $t('default.oil') }}</th>
-            <th>{{ $t('default.price') }}</th>
-            <th>{{ $t('default.year') }}</th>
-            <th>{{ $t('default.engine') }}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="car in carList" :key="car.carID">
-            <td>{{ verifyString(car.brand) }}</td>
-            <td>{{ verifyString(car.name) }}</td>
-            <td>{{ verifyString(car.carType) }}</td>
-            <td>{{ verifyString(car.driveSystem) }}</td>
-            <td>{{ verifyString(car.gearType) }}</td>
-            <td>{{ verifyString(car.oilType) }}</td>
-            <td>{{ verifyString(car.price).toLocaleString() }}</td>
-            <td>{{ verifyString(car.year) }}</td>
-            <td>{{ verifyString(car.engine) }}</td>
-          </tr>
-        </tbody>
-      </table>
+    <div>
+        <Search />
+        <div class="table-box">
+            <table class="table table-striped table-bordered">
+                <thead >
+                    <tr>
+                        <th>{{ $t('default.brand') }}</th>
+                        <th>{{ $t('default.name') }}</th>
+                        <th>{{ $t('default.carType') }}</th>
+                        <th>{{ $t('default.driveSystem') }}</th>
+                        <th>{{ $t('default.gearType') }}</th>
+                        <th>{{ $t('default.oil') }}</th>
+                        <th>{{ $t('default.price') }}</th>
+                        <th>{{ $t('default.year') }}</th>
+                        <th>{{ $t('default.engine') }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="car in carList" :key="car.carID">
+                        <td>{{ verifyString(car.brand) }}</td>
+                        <td>{{ verifyString(car.name) }}</td>
+                        <td>{{ verifyString(car.carType) }}</td>
+                        <td>{{ verifyString(car.driveSystem) }}</td>
+                        <td>{{ verifyString(car.gearType) }}</td>
+                        <td>{{ verifyString(car.oilType) }}</td>
+                        <td>{{ verifyString(car.price).toLocaleString() }}</td>
+                        <td>{{ verifyString(car.year) }}</td>
+                        <td>{{ verifyString(car.engine) }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <AppFooter />
     </div>
-    <AppFooter />
-  </div>
 </template>
 <script>
 import { createNamespacedHelpers } from 'vuex'
 import Search from '@/components/SearchCar.vue'
 import AppFooter from '@/components/Footer.vue';
-import { isInteger } from 'lodash'
-import { request } from '@/services/api';
-import axios from 'axios';
+import { isInteger} from 'lodash'
 const { mapState: mapCarState } = createNamespacedHelpers('car')
 export default {
-  computed: {
-    ...mapCarState({
-      carList: state => state.carList
-    })
-  },
-  data() {
-    return {
-      img: ''
-    }
-  },  
-  methods: {
-    verifyString(str) {
-      if (isInteger(str)) {
-        return str
-      }
-      if (this.$isEmpty(str)) {
-        return "-";
-      }
-      return str;
+    computed: {
+        ...mapCarState({
+            carList: state => state.carList
+        })
     },
-    async getImg() {
-      const result = await axios.get('')
-      this.img = result
-    }
-  },
-  components: { Search, AppFooter }
+    methods: {
+        verifyString(str) {
+            if(isInteger(str)){
+                return str
+            }
+            if (this.$isEmpty(str)) {
+                return "-";
+            }
+            return str;
+        }
+    },
+    components: { Search, AppFooter }
 }
 </script>
 <style scoped>
-#img-1 {
-  image-orientation: from-image;
-}
 </style>
--- src/views/error/ErrorConnection.vue
+++ src/views/error/ErrorConnection.vue
@@ -1,39 +1,13 @@
 <template>
     <div>
-        <img id="img-1" :src="img" />
-        <!-- <BaseInfo imgSrc="warning.png" :title="$t('error.internet')" /> -->
+        <BaseInfo imgSrc="warning.png" :title="$t('error.internet')" />
     </div>
 </template>
 <script>
 import BaseInfo from '@/components/BaseInfo.vue'
-import axios from 'axios';
-import { rotate, tranfromImage } from '@/utils/imageUtils';
 export default {
-    components: { BaseInfo },
-    data() {
-        return {
-            img: ""
-        }
-    },
-    methods: {
-        async getImg() {
-            console.log('check');
-            const result = await axios.post("http://localhost:8444/a").then().catch()
-            rotate(`data:image/jpeg;base64,${result.data.result}`,(base64)=>this.img=base64)
-            // this.img = `data:image/jpeg;base64,${result.data.result}`
-            // console.log(this.img)
-        }
-    },
-    mounted(){
-        this.getImg();
-    }
+    components: { BaseInfo }
 }
-
 </script>
 <style>
-#img-1 {
-    image-orientation: none;
-    /* width: 400px;
-    height: 400px; */
-}
 </style>
